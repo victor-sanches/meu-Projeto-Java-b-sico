9f3bfafde970afed3c50d7c3f979496383fbe0e2
joao.ferreira <joaovictrsanches0902@gmail.com>
2025-04-11 01:00:01 -0400

    Atualização-TiposVariaveis

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos ptimitivos
+        //estude a class String que representa texto e aplicação
+
+        String meuNome = "João Victor"
+
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3,14;
     }
 }
