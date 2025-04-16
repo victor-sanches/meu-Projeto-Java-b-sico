830a995b50731721b11060b76fbbccc3047dc383
joao.ferreira <joaovictrsanches0902@gmail.com>
2025-04-15 22:36:30 -0400

    Atualização

---
+++ TIPOS-VARIAVEIS/src/tiposVariaveis.java
@@ -0,0 +1,25 @@
+public class tiposVariaveis {
+        public static void main(String[] args) {
+    
+            //Tipos primitivos
+    
+            int idade = 22;              // Número inteiro
+            double salario = 2000.50;   // Número decimal
+            char letra = 'J';           // Um único coractere
+            boolean isAtivo = true;   //  Valor lógico
+    
+            // Tipo de Referência
+    
+            String nome = "João Victor Sanches"; // Sequência de caracteres (texto)
+    
+            // Exibindo valores das variáveis
+    
+            System.out.println("Nome: " + nome);
+            System.out.println("Idade: " + idade);
+            System.out.println("Salário: " + salario);
+            System.out.println("Premeira letra do nome: " + letra);
+            System.out.println("Ativo: " + isAtivo);
+
+    }
+    
+}
